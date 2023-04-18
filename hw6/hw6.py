05352eafd9c3ed397efa862f1b7c062b59ff7e45
Valery Alexeev <valery@MacBook-Pro-Valery.local>
2023-04-18 17:22:29 +0300

    hw6 done

---
+++ hw6/hw6.py
@@ -0,0 +1,18 @@
+# 1
+
+n = int(input('Input n ')) 
+x = int(input('Input a[0] '))
+d = int(input('Input d ')) 
+print(*range(x, x + d * n, d))
+
+# for i in range(n):
+    # print(x + i * d)
+
+# 2
+
+list_1 = [-5, 9, 0, 3, -1, -2, 1, 4, -2, 10, 2, 0, -9, 8, 10, -9, 0, -5, -5, 7]
+min_number = int(input())
+max_number = int(input())
+for i in range(len(list_1)):
+    if min_number <= list_1[i] <= max_number:
+        print(i)
